8cadb4381e4ef0e2c06a37d87a66f985057c3e03
cseibert2667 <cseibert2667@gmail.com>
2020-09-02 14:57:02 -0700

    basic server that displays homepage

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
+
+app.get("/", function (req,res) {
+    res.sendFile(__dirname + "/public/index.html")
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
