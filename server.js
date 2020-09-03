99ef365705ba2fafbea75132d035e01c5748c578
cseibert2667 <cseibert2667@gmail.com>
2020-09-02 21:17:43 -0700

    got notes.html to load external css/js files

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
-const logger = require("morgan")
+const path = require("path");
+const logger = require("morgan");
+const fs = require("fs");
 
-var app = express();
-var PORT = 3000;
+let app = express();
+let PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
+app.use(express.static("public"))
 
-app.get("/", function (req,res) {
+app.get("/notes", function(req,res){
+  res.sendFile(path.join(__dirname + "/public/notes.html"))
+})
+
+app.get("*", function (req,res) {
     res.sendFile(__dirname + "/public/index.html")
 });
 
