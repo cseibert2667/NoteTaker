56466d55fd337730a1f4e1478bbbb0bd5646b7dc
cseibert2667 <cseibert2667@gmail.com>
2020-09-02 22:04:46 -0700

    added routes for redering saved notes & saving new notes

--- server.js
+++ server.js
@@ -6,20 +6,39 @@ const fs = require("fs");
 
 let app = express();
 let PORT = 3000;
-
+let savedNotes = []
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
-app.use(express.static("public"))
-
+app.use(express.static("public"));
+// route to get saved notes
+app.get("/api/notes", function(req,res){
+  savedNotes = fs.readFileSync(__dirname + "/db/db.json", "utf8");
+  savedNotes = JSON.parse(savedNotes);
+  res.json(savedNotes);
+});
+// route to save a note (also assigns unique id)
+app.post("/api/notes", function(req,res){
+  const note = req.body;
+  note.id = (uuidv4());
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data){
+    const savedNotes = JSON.parse(data);
+    savedNotes.push(note);
+    const stringified = JSON.stringify(savedNotes, null, 2);
+    fs.writeFile(__dirname + "/db/db.json", stringified, function () {
+      res.json(note)
+    })
+  })
+})
+// route to load notes.html
 app.get("/notes", function(req,res){
   res.sendFile(path.join(__dirname + "/public/notes.html"))
-})
-
+});
+// route to load home page
 app.get("*", function (req,res) {
     res.sendFile(__dirname + "/public/index.html")
 });
-
+// server listener
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
